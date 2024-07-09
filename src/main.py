56227c14846beb3e5656a3c994da993e84bd9a5b
ルチルさん <29337073+Rutile3@users.noreply.github.com>
2024-07-09 22:43:13 +0900

    【機能追加】複数ファイルの指定に対応

--- src/main.py
+++ src/main.py
@@ -5,18 +5,18 @@ from unzipper import is_zip_file, unzip_file
 def main():
     # コマンドライン引数の設定
     parser = argparse.ArgumentParser(description="zipファイルを解凍します。")
-    parser.add_argument("zip_file_path", type=str, help="zipファイルのパス。")
+    parser.add_argument('input_files', nargs='+', help='解凍するzipファイルのパス。')
 
     # コマンドライン引数の取得
     args = parser.parse_args()
-    zip_file_path = args.zip_file_path
+    input_files = args.input_files
 
     # zipファイルなら解凍する
-    if is_zip_file(zip_file_path):
-        unzip_file(zip_file_path)
-    else:
-        print("zipファイルを指定してください。")
-
+    for input_item in input_files:
+        if is_zip_file(input_item):
+            unzip_file(input_item)
+        else:
+            print(f"「{input_item}」はzipファイルをではありません。")
 
 # mainの関数呼び出し
 if __name__ == "__main__":
